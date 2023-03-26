912f84f5f2b07e13319df913653206f288b70724
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-03-27 07:06:41 +1100

    Removed github workflow for gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -15,7 +17,7 @@ const config = {
 		// Ignore above comment, now using adapter-static:
 		adapter: adapter({ strict: false }),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/family-admin' : ''
+			base: dev ? '/family-admin' : ''
 		}
 	}
 };
