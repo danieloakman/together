edfbf4338cdcfd6f1cbd72fbd38220068bc30194
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-04-14 00:22:23 +1000

    Added capacitor to handle PWA

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fabmin.app',
+  appName: 'Fabmin',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
