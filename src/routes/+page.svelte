234a7297751d52b864690ccd098324d175b4ca03
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-04-17 00:10:48 +1000

    Added /android/

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,19 @@
-<script></script>
+<script lang="ts">
+	import { Geolocation } from '@capacitor/geolocation';
+  import { Filesystem } from '@capacitor/filesystem';
+
+  function sleep(time: number) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
+
+  sleep(2000).then(async () => {
+    const position = await Geolocation.getCurrentPosition();
+    console.log(position.coords.latitude, position.coords.longitude);
+    const perm = await Filesystem.checkPermissions();
+    console.log({ perm });
+  })
+
+</script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
