ff6c2dbcf67ae378aee7abe1243ed8cb2282bd40
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-04-17 13:12:25 +1000

    Added ionic

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { setupIonicBase, setupIonicSvelte } from 'ionic-svelte';
+
+	/* run base configuration code from ionic/core */
+	setupIonicBase();
+
+	/* load and register all components - you can also import separately to code split */
+	// import 'ionic-svelte/components/all';
+	import 'ionic-svelte/components/ion-app';
+	import 'ionic-svelte/components/ion-card';
+	import 'ionic-svelte/components/ion-card-title';
+	import 'ionic-svelte/components/ion-card-subtitle';
+	import 'ionic-svelte/components/ion-card-header';
+	import 'ionic-svelte/components/ion-card-content';
+	import 'ionic-svelte/components/ion-chip';
+	import 'ionic-svelte/components/ion-button';
+
+	/* Theme variables */
+	import '../theme/variables.css';
+</script>
+
+<ion-app>
+	<slot />
+</ion-app>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,40 @@
 <script lang="ts">
 	import { Geolocation } from '@capacitor/geolocation';
-  import { Filesystem } from '@capacitor/filesystem';
-  import { onMount } from 'svelte';
+	import { Filesystem } from '@capacitor/filesystem';
+	import { onMount } from 'svelte';
+	// interface IonButton {
+	// 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	// 	[key: string]: any;
+	// }
 
-  function sleep(time: number) {
-    return new Promise((resolve) => setTimeout(resolve, time));
-  }
+	function sleep(time: number) {
+		return new Promise((resolve) => setTimeout(resolve, time));
+	}
 
-  onMount(async () => {
-    const position = await Geolocation.getCurrentPosition();
-    console.log(position.coords.latitude, position.coords.longitude);
-    const perm = await Filesystem.checkPermissions();
-    console.log({ perm });
-  });
+	onMount(async () => {
+		const position = await Geolocation.getCurrentPosition();
+		console.log(position.coords.latitude, position.coords.longitude);
+		const perm = await Filesystem.checkPermissions();
+		console.log({ perm });
+	});
 
-  let count = 1;
-  function increment() {
-    count += 1;
-  }
+	let count = 1;
+	function increment() {
+		count += 1;
+	}
+
+	async function test() {
+		const perm = await Filesystem.checkPermissions();
+		console.log({ perm });
+	}
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>cool ok</p>
-<button on:click={increment}>Count +{count}</button>
-<br />
-<button on:click={async () => {
-  const perm = await Filesystem.checkPermissions();
-  console.log({ perm });
-}}>Test</button>
+<ion-card>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<p>cool ok</p>
+	<button on:click={increment}>Count +{count}</button>
+	<br />
+	<slot />
+	<ion-button on:keydown={test} on:click={test}>Test</ion-button>
+</ion-card>
