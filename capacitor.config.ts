ced9abcbe881e8cd9a2879a6b5d83f7b4f663caf
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-04-17 12:43:08 +1000

    Added some capacitor config ease of use

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,23 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
+// TODO: find a better way to turn live reloading on/off via cli or env var:
+// /** If true then live reloading is turned on. */
+// const LIVE_RELOAD = process.argv.includes('-lr') || process.argv.includes('--live-reloading');
+
+const server: CapacitorConfig['server'] = { 
+  url: 'http://10.0.2.2:5173',
+  cleartext: true,
+};
+// const server: CapacitorConfig['server'] = undefined;
+
 const config: CapacitorConfig = {
   appId: 'com.fabmin.app',
   appName: 'Fabmin',
   webDir: 'build',
   bundledWebRuntime: false,
-  server: {
-    url: 'http://10.0.2.2:5173',
-    cleartext: true,
-  }
+  server
 };
+
+if (server) console.log(`🔴🔥 Running capactitor with live-reload at "${server?.url}"`);
 
 export default config;
