2f29983ced5c8df179ad5f7fe249f6fcf9a67780
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-05-06 17:01:09 +1000

    Added some segments

--- src/routes/lists/+page.svelte
+++ src/routes/lists/+page.svelte
@@ -1,3 +1,25 @@
+<script lang="ts">
+	function segmentChange(event: any) {
+		currentSegment = event.detail.value;
+	}
+	type Segment = 'Shopping' | 'Todos';
+	let currentSegment: Segment = 'Shopping';
+
+</script>
+
 <ion-tab tab="lists">
-	<ion-card><ion-text>Hello</ion-text></ion-card>
+	<ion-segment value="Shopping" on:ionChange={segmentChange}>
+		<ion-segment-button value="Shopping">
+			<ion-label>Shopping</ion-label>
+		</ion-segment-button>
+		<ion-segment-button value="Todos">
+			<ion-label>Todos</ion-label>
+		</ion-segment-button>
+	</ion-segment>
+	{#if currentSegment === 'Shopping'}
+		<ion-content class="ion-padding">Shopping</ion-content>
+	{/if}
+	{#if currentSegment === 'Todos'}
+		<ion-content class="ion-padding">Todos</ion-content>
+	{/if}
 </ion-tab>
